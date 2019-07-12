7a1f1db4866ef31d24acf6da74833c4c701d22f4
Ami Kehaty <amiramke@gmail.com>
2019-07-12 16:12:29 -0700

    wip

--- lib/nanomart.rb
+++ lib/nanomart.rb
@@ -1,86 +1,55 @@
 # you can buy just a few things at this nanomart
-require 'highline'
 
 class Nanomart
   class NoSale < StandardError; end
 
-  def initialize(logfile, prompter)
-    @logfile, @prompter = logfile, prompter
+  def initialize(logfile)
+    @logfile = logfile
   end
 
-  def sell_me(itm_type)
+  def sell_me(itm_type, prompter)
     itm = case itm_type
           when :beer
-            Item::Beer.new(@logfile, @prompter)
+            Item::Beer.new(@logfile)
           when :whiskey
-            Item::Whiskey.new(@logfile, @prompter)
+            Item::Whiskey.new(@logfile)
           when :cigarettes
-            Item::Cigarettes.new(@logfile, @prompter)
+            Item::Cigarettes.new(@logfile)
           when :cola
-            Item::Cola.new(@logfile, @prompter)
+            Item::Cola.new(@logfile)
           when :canned_haggis
-            Item::CannedHaggis.new(@logfile, @prompter)
+            Item::CannedHaggis.new(@logfile)
           else
             raise ArgumentError, "Don't know how to sell #{itm_type}"
           end
 
-    itm.rstrctns.each do |r|
-      itm.try_purchase(r.ck)
+    itm.restrictions.each do |r|
+      itm.try_purchase(r.check(prompter))
     end
     itm.log_sale
   end
 end
-
-class HighlinePrompter
-  def get_age
-    HighLine.new.ask('Age? ', Integer) # prompts for user's age, reads it in
-  end
-end
-
 
 module Restriction
   DRINKING_AGE = 21
   SMOKING_AGE = 18
 
   class DrinkingAge
-    def initialize(p)
-      @prompter = p
-    end
-
-    def ck
-      age = @prompter.get_age
-      if age >= DRINKING_AGE
-        true
-      else
-        false
-      end
+    def check(prompter)
+      prompter.get_age >= DRINKING_AGE
     end
   end
 
   class SmokingAge
-    def initialize(p)
-      @prompter = p
-    end
-
-    def ck
-      age = @prompter.get_age
-      if age >= SMOKING_AGE
-        true
-      else
-        false
-      end
+    def check(prompter)
+      prompter.get_age >= SMOKING_AGE
     end
   end
 
   class SundayBlueLaw
-    def initialize(p)
-      @prompter = p
-    end
-
-    def ck
-      # pp Time.now.wday
-      # debugger
-      Time.now.wday != 0      # 0 is Sunday
+    def check(_prompter)
+      # 0 is Sunday
+      Time.now.wday != 0
     end
   end
 end
@@ -88,22 +57,23 @@ end
 class Item
   INVENTORY_LOG = 'inventory.log'
 
-  def initialize(logfile, prompter)
-    @logfile, @prompter = logfile, prompter
+  def initialize(logfile)
+    @logfile = logfile
   end
 
   def log_sale
     File.open(@logfile, 'a') do |f|
-      f.write(nam.to_s + "\n")
+      f.write(name.to_s + "\n")
     end
   end
 
-  def nam
-    class_string = self.class.to_s
-    short_class_string = class_string.sub(/^Item::/, '')
-    lower_class_string = short_class_string.downcase
-    class_sym = lower_class_string.to_sym
-    class_sym
+  def name
+    self.
+      class.
+      to_s.
+      sub(/^Item::/, '').
+      downcase.
+      to_sym
   end
 
   def try_purchase(success)
@@ -115,38 +85,41 @@ class Item
   end
 
   class Beer < Item
-    def rstrctns
-      [Restriction::DrinkingAge.new(@prompter)]
+    def restrictions
+      [Restriction::DrinkingAge.new]
     end
   end
 
   class Whiskey < Item
     # you can't sell hard liquor on Sundays for some reason
-    def rstrctns
-      [Restriction::DrinkingAge.new(@prompter), Restriction::SundayBlueLaw.new(@prompter)]
+    def restrictions
+      [
+        Restriction::DrinkingAge.new,
+        Restriction::SundayBlueLaw.new
+      ]
     end
   end
 
   class Cigarettes < Item
     # you have to be of a certain age to buy tobacco
-    def rstrctns
-      [Restriction::SmokingAge.new(@prompter)]
+    def restrictions
+      [Restriction::SmokingAge.new]
     end
   end
 
   class Cola < Item
-    def rstrctns
+    def restrictions
       []
     end
   end
 
   class CannedHaggis < Item
-    # the common-case implementation of Item.nam doesn't work here
-    def nam
+    # the common-case implementation of Item.name doesn't work here
+    def name
       :canned_haggis
     end
 
-    def rstrctns
+    def restrictions
       []
     end
   end
--- spec/nanomart_spec.rb
+++ spec/nanomart_spec.rb
@@ -15,61 +15,61 @@ end
 describe "making sure the customer is old enough" do
   context "when you're a kid" do
     before(:each) do
-      @nanomart = Nanomart.new('/dev/null', Age9.new)
+      @nanomart = Nanomart.new('/dev/null')
     end
 
     it "lets you buy cola and canned haggis" do
-      expect { @nanomart.sell_me(:cola)          }.not_to raise_error
-      expect { @nanomart.sell_me(:canned_haggis) }.not_to raise_error
+      expect { @nanomart.sell_me(:cola, Age9.new)          }.not_to raise_error
+      expect { @nanomart.sell_me(:canned_haggis, Age9.new) }.not_to raise_error
     end
 
     it "stops you from buying anything age-restricted" do
-      expect { @nanomart.sell_me(:beer)       }.to raise_error(Nanomart::NoSale)
-      expect { @nanomart.sell_me(:whiskey)    }.to raise_error(Nanomart::NoSale)
-      expect { @nanomart.sell_me(:cigarettes) }.to raise_error(Nanomart::NoSale)
+      expect { @nanomart.sell_me(:beer, Age9.new)       }.to raise_error(Nanomart::NoSale)
+      expect { @nanomart.sell_me(:whiskey, Age9.new)    }.to raise_error(Nanomart::NoSale)
+      expect { @nanomart.sell_me(:cigarettes, Age9.new) }.to raise_error(Nanomart::NoSale)
     end
   end
 
   context "when you're a newly-minted adult" do
     before(:each) do
-      @nanomart = Nanomart.new('/dev/null', Age19.new)
+      @nanomart = Nanomart.new('/dev/null')
     end
 
     it "lets you buy cola, canned haggis, and cigarettes (to hide the taste of the haggis)" do
-      expect { @nanomart.sell_me(:cola)          }.not_to raise_error
-      expect { @nanomart.sell_me(:canned_haggis) }.not_to raise_error
-      expect { @nanomart.sell_me(:cigarettes)    }.not_to raise_error
+      expect { @nanomart.sell_me(:cola, Age19.new)          }.not_to raise_error
+      expect { @nanomart.sell_me(:canned_haggis, Age19.new) }.not_to raise_error
+      expect { @nanomart.sell_me(:cigarettes, Age19.new)    }.not_to raise_error
     end
 
     it "stops you from buying anything age-restricted" do
-      expect { @nanomart.sell_me(:beer)       }.to raise_error(Nanomart::NoSale)
-      expect { @nanomart.sell_me(:whiskey)    }.to raise_error(Nanomart::NoSale)
+      expect { @nanomart.sell_me(:beer, Age19.new)       }.to raise_error(Nanomart::NoSale)
+      expect { @nanomart.sell_me(:whiskey, Age19.new)    }.to raise_error(Nanomart::NoSale)
     end
   end
 
   context "when you're an old fogey on Thursday" do
     before(:each) do
-      @nanomart = Nanomart.new('/dev/null', Age99.new)
+      @nanomart = Nanomart.new('/dev/null')
       allow(Time).to receive(:now).and_return(Time.local(2010, 8, 12, 12))  # Thursday Aug 12 2010 12:00
     end
 
     it "lets you buy everything" do
-      expect { @nanomart.sell_me(:cola)          }.not_to raise_error
-      expect { @nanomart.sell_me(:canned_haggis) }.not_to raise_error
-      expect { @nanomart.sell_me(:cigarettes)    }.not_to raise_error
-      expect { @nanomart.sell_me(:beer)          }.not_to raise_error
-      expect { @nanomart.sell_me(:whiskey)       }.not_to raise_error
+      expect { @nanomart.sell_me(:cola, Age99.new)          }.not_to raise_error
+      expect { @nanomart.sell_me(:canned_haggis, Age99.new) }.not_to raise_error
+      expect { @nanomart.sell_me(:cigarettes, Age99.new)    }.not_to raise_error
+      expect { @nanomart.sell_me(:beer, Age99.new)          }.not_to raise_error
+      expect { @nanomart.sell_me(:whiskey, Age99.new)       }.not_to raise_error
     end
   end
 
   context "when you're an old fogey on Sunday" do
     before(:each) do
-      @nanomart = Nanomart.new('/dev/null', Age99.new)
+      @nanomart = Nanomart.new('/dev/null')
       allow(Time).to receive(:now).and_return(Time.local(2010, 8, 15, 12))  # Sunday Aug 15 2010 12:00
     end
 
     it "stops you from buying hard alcohol" do
-      expect { @nanomart.sell_me(:whiskey)       }.to raise_error(Nanomart::NoSale)
+      expect { @nanomart.sell_me(:whiskey, Age99.new)       }.to raise_error(Nanomart::NoSale)
     end
   end
 end
